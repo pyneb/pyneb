0aa6e17e433d9a7c5ae430348d4cd739ac16b828
eflynn67 <flynner6@msu.edu>
2021-08-17 16:06:13 -0400

    adding place holder functions

--- py_neb/py_neb.py
+++ py_neb/py_neb.py
@@ -1,1 +1,51 @@
 import numpy as np
+from scipy.ndimage import filters, morphology #For minimum finding
+import h5py
+
+def find_local_minimum(arr):
+    """
+    Returns the indices corresponding to the local minimum values. Taken 
+    directly from https://stackoverflow.com/a/3986876
+    
+    Parameters
+    ----------
+    arr : Numpy array
+        A D-dimensional array.
+
+    Returns
+    -------
+    minIndsOut : Tuple of numpy arrays
+        D arrays of length k, for k minima found
+
+    """
+    neighborhood = morphology.generate_binary_structure(len(arr.shape),1)
+    local_min = (filters.minimum_filter(arr, footprint=neighborhood,\
+                                        mode="nearest")==arr)
+    
+    background = (arr==0)
+    #Not sure this is necessary - it doesn't seem to do much on the test
+        #data I defined.
+    eroded_background = morphology.binary_erosion(background,\
+                                                  structure=neighborhood,\
+                                                  border_value=1)
+        
+    detected_minima = local_min ^ eroded_background
+    allMinInds = np.vstack(local_min.nonzero())
+    minIndsOut = tuple([allMinInds[coordIter,:] for \
+                        coordIter in range(allMinInds.shape[0])])
+    return minIndsOut
+
+## Need include target function option
+def grad():
+    return()
+
+### Need mass parameter
+def action(path,pot,minimum):
+    return()
+
+class Least_action_path_NEB():
+    
+class Minimum_energy_path_NEB():
+
+
+    
