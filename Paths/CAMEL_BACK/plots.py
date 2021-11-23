55eb4c85db41af9f3a06ab5b57b838203a3c933c
eflynn67 <flynner6@msu.edu>
2021-11-22 22:53:29 -0500

    saving plot script

--- Paths/CAMEL_BACK/plots.py
+++ Paths/CAMEL_BACK/plots.py
@@ -5,7 +5,6 @@ import time
 import sys
 
 ### add pyneb
-import sys
 sys.path.insert(0, '../../py_neb/py_neb')
 sys.path.insert(0, '../../flynn_code/py_neb_demos')
 import utilities
@@ -30,7 +29,7 @@ def camel_back(coords):
     else:pass
     result = (4 - 2.1*(x**2) + (1/3) * (x**4))*x**2 + x*y + 4*((y**2) - 1)*(y**2) 
     return(result)
-def camel_back_broken_sym(coords):
+def camel_back_asymm(coords):
     if isinstance(coords, np.ndarray)==False:
         coords = np.array(coords)
     ## check if it's scalar
@@ -61,13 +60,42 @@ paths = {'LAP': LAP_path,'MEP': MEP_path,'DP': DP_path}
 path_names = paths.keys()
 action_dict = {}
 #Define potential function
-V_func = camel_back_broken_sym
+V_func = camel_back_asymm
+
+### Find global minimum to shift surface
+x = np.linspace(-2, 2,300)
+y = np.linspace(-1.25, 1.25,300)
+uniq_coords = [x,y]
+
+xx,yy = np.meshgrid(x,y)
+grids = [xx,yy]
+coords = np.array([xx,yy])
+EE_0 = V_func(np.array([xx,yy]))
+
+#Calculate minima of PES
+minima_id =  utilities.SurfaceUtils.find_all_local_minimum(EE_0)
+minima_coords = np.array((xx[minima_id],yy[minima_id])).T
+V_min = V_func(minima_coords)
+sorted_ascending_idx = np.argsort(V_min) #places global min first
+
+# redefine minima_coords so that they are in ascending order
+minima_coords = minima_coords[sorted_ascending_idx]
+V_min = V_func([minima_coords[:,0],minima_coords[:,1]])
+####
+gs_coord = minima_coords[0]
+E_gs = V_func(gs_coord)
+print('E_gs: ',E_gs)
+V_func_shift = utilities.shift_func(V_func,shift=E_gs)#shift by the ground state
+EE = V_func_shift(np.array([xx,yy]))
+
+
+
 
 ## Interpolate the paths 
 for name in path_names:
     path = paths[name]
     path_call = utilities.InterpolatedPath(path)
-    action_dict[name] = np.around(path_call.compute_along_path(utilities.TargetFunctions.action,1000,tfArgs=[V_func])[1][0],3)
+    action_dict[name] = np.around(path_call.compute_along_path(utilities.TargetFunctions.action,500,tfArgs=[V_func_shift])[1][0],3)
 
 for name in action_dict.keys():
     print(name+': ', action_dict[name])
@@ -86,8 +114,8 @@ EE = V_func(np.array([xx,yy]))
 
 
 fig, ax = plt.subplots(1,1,figsize = (8, 6))
-im = ax.contourf(grids[0],grids[1],EE,cmap='Spectral_r',extend='both',levels=MaxNLocator(nbins = 200).tick_values(-1,4))
-ax.contour(grids[0],grids[1],EE,colors=['black'],levels=MaxNLocator(nbins = 15).tick_values(-1,4))  
+im = ax.contourf(grids[0],grids[1],EE,cmap='Spectral_r',extend='both',levels=MaxNLocator(nbins = 200).tick_values(0,4))
+ax.contour(grids[0],grids[1],EE,colors=['black'],levels=MaxNLocator(nbins = 15).tick_values(0,4))  
 ax.plot(LAP_path[:, 0], LAP_path[:, 1],label='LAP '+str(action_dict['LAP']),linestyle='-',marker='.',color='purple',linewidth=1.0,markersize=6)
 ax.plot(MEP_path[:, 0], MEP_path[:, 1],label='MEP '+str(action_dict['MEP']),linestyle='-',marker='.',color='red',linewidth=1.0,markersize=6)
 ax.plot(DP_path[:, 0], DP_path[:, 1],label='DPM '+str(action_dict['DP']),linestyle='-',marker='.',color='black',linewidth=1.0,markersize=6)
