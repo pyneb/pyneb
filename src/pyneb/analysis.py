3a6552a59ad446c922a817de7b44d33cbac6692e
dlay97 <laydanie@msu.edu>
2022-07-12 16:52:49 -0400

    Added tentative code for selecting unique paths, given a list of paths

--- src/pyneb/analysis.py
+++ src/pyneb/analysis.py
@@ -1,27 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
 
-from utilities import *
-from fileio import *
+import utilities
+import fileio
 
-"""
-Plots to make:
-    -Action vs iterations
-    -Path(s) on PES/inertia components
-    -PES/inertia along path (1D)
-    -Raw PES/inertia
-    -Cumulative action?
+import warnings
 
-Want to just feed in a list of log files, and make the plots from there. Can
-also be compatible with feeding in a class instance, in e.g. LAP cases
-"""
-
-def action_vs_iteration(inputObj,enegFunc,massFunc=None,
-                        target_func=TargetFunctions.action,interp=False,
-                        nImages=None,interpKWargs={},figName=None):
-    fig, ax = plt.subplots()
+def cluster_paths_by_endpoints(listOfPaths,dbscanParams={}):
+    warnings.warn("Method still in development")
     
-    if isinstance(inputObj,str):
-        inputObj = LoadForceLogger(inputObj)
+    defaultDbscanParams = {"eps":0.1,"min_samples":1}
+    for (key,val) in defaultDbscanParams.items():
+        if key not in dbscanParams:
+            dbscanParams[key] = val
     
-    return fig, ax
+    endPoints = np.array([p[-1] for p in listOfPaths])
+    
+    rescaledPoints = StandardScaler().fit_transform(endPoints)
+    db = DBSCAN(**dbscanParams).fit(rescaledPoints)
+    labels = np.array(db.labels_)
+    nClusters = len(set(labels)) - (1 if -1 in labels else 0)
+    
+    uniquePaths = []
+    uniqueInds = []
+    for i in range(nClusters):
+        idx = np.where(labels==i)[0][0]
+        uniqueInds.append(idx)
+        uniquePaths.append(listOfPaths[idx])
+    
+    return uniquePaths, uniqueInds
+
+def find_most_similar_paths(firstList,secondList):
+    """
+    For every path in firstList, finds the path in secondList that is the closest.
+    
+    Does
+    not do anything more sophisticated, like check if the paths 'look like'
+    each other, because that's prohibitively complicated (I don't know how to
+    do it).
+    
+    Allows for lists of different lengths.
+
+    Parameters
+    ----------
+    firstList : TYPE
+        DESCRIPTION.
+    secondList : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    warnings.warn("Method still in development")
+    
+    distancesDict = {}
+    nearestIndsDict = {}
+    for (p1Iter,p1) in enumerate(firstList):
+        e1 = p1[-1]
+        distancesDict[p1Iter] = np.inf
+        for (p2Iter,p2) in enumerate(secondList):
+            e2 = p2[-1]
+            dist = np.linalg.norm(e2-e1)
+            if dist < distancesDict[p1Iter]:
+                distancesDict[p1Iter] = dist
+                nearestIndsDict[p1Iter] = p2Iter
+    
+    return nearestIndsDict, distancesDict
+    
