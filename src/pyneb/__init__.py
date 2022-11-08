a2fec64af26bf7a9d3ae96c46a02b6a868e7518c
dlay97 <laydanie@msu.edu>
2022-11-08 15:10:09 -0500

    Updated __init__.py to prevent importing analysis.py file from working directory

--- src/pyneb/__init__.py
+++ src/pyneb/__init__.py
@@ -4,9 +4,9 @@ dirName = os.path.dirname(os.path.realpath(__file__))
 if dirName not in sys.path:
     sys.path.append(dirName)
 
-from solvers import *
-from utilities import *
-from analysis import *
-from fileio import *
+from .solvers import *
+from .utilities import *
+from .analysis import *
+from .fileio import *
 
-import solvers, utilities, analysis, fileio
+
