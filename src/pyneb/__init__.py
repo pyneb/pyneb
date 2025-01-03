04b15ea3420797f29a2c7d99407931cbc3d82bb9
dlay97 <dlay@terpmail.umd.edu>
2025-01-03 13:00:40 -0500

    import now checks for utils folder in pyneb directory first, before checking rest of sys.path

--- src/pyneb/__init__.py
+++ src/pyneb/__init__.py
@@ -2,7 +2,7 @@ import os, sys
 #Taken from https://stackoverflow.com/a/49375740
 dirName = os.path.dirname(os.path.realpath(__file__))
 if dirName not in sys.path:
-    sys.path.append(dirName)
+    sys.path.insert(0,dirName)
 
 from .solvers import *
 from .utilities import *
